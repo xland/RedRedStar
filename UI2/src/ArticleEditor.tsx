d704f1e5f88ffc7ff768be39a376ab5d3bd4a641
liulun <xland@live.cn>
2026-09-05 19:41:15 +0800

    Editor

--- UI2/src/ArticleEditor.tsx
+++ UI2/src/ArticleEditor.tsx
@@ -1,4 +1,23 @@
 import "./ArticleEditor.scss";
+import type { IEditor } from "roosterjs";
+import { Editor } from "roosterjs-content-model-core";
+import { WatermarkPlugin } from "roosterjs";
+import msg from "./msg";
 export default function ArticleEditor() {
-  return <div id="articleEditor"></div>;
+  msg.once("ready", () => {
+    const contentDiv = document.querySelector<HTMLDivElement>("#articleContent")!;
+    const editor = new Editor(contentDiv, {
+      plugins: [new WatermarkPlugin("请输入文章内容…")],
+      defaultSegmentFormat: {
+        fontFamily: "微软雅黑",
+        fontSize: "15px",
+      },
+    });
+  });
+
+  return (
+    <div id="articleEditor">
+      <div id="articleContent"></div>
+    </div>
+  );
 }
